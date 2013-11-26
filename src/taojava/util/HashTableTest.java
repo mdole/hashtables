7064158d190ea9c502e51dc98782eb7d6c6610cd
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-11-26 09:26:35 -0600

    Add blank unit test class.

---
+++ src/taojava/util/HashTableTest.java
@@ -0,0 +1,17 @@
+package taojava.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for our hash table class.  Included for future use.
+ */
+public class HashTableTest {
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    } // test
+
+ } // HashTableTest
